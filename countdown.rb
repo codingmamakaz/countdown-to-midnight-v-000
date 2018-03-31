a5c262872dfa4d0da166d8f5a7f688faef403be2
Kazumi Karbowski <kazkazinus@gmail.com>
2018-03-31 02:24:55 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,7 @@ def countdown(number)
 end
 
 
+<<<<<<< HEAD
 def countdown_with_sleep(number)
   while number > 0
   sleep 1
@@ -16,4 +17,11 @@ def countdown_with_sleep(number)
     number -= 1
   end
   return "HAPPY NEW YEAR!"
+=======
+def countdown_with_sleep(i)
+  while i > 0
+    sleep 1
+    i -= 1
+  end
+>>>>>>> 3fcea96aca759d27582dcd59aa56c66584fc693a
 end
